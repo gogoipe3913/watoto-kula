bba8d15ca548a1cbe13d0ddaaab3a7526fd53a4c
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2025-02-02 12:58:34 +0900

    masterブランチは準備中にしておく

--- src/app/stay/page.tsx
+++ src/app/stay/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import styles from "./page.module.scss";
+
 export default function Stay() {
-  return <div>準備中です。</div>;
+  return (
+    <div className={styles.Stay}>
+      <br />
+      Watoto Stay Litaは、
+      <br />
+      福井県小浜市忠野9-14 にて2025年春よりオープン予定です。
+      <br />
+      今しばらくお待ちください。
+    </div>
+  );
 }
