e752920dfbb08af962ef8b8333695508a3ad470c
Taiki Kishiyama <taiki.kishiyama0913@gmail.com>
2025-03-04 00:56:59 +0900

    favicon修正

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon.ico" />
         <script
           dangerouslySetInnerHTML={{
             __html: `
